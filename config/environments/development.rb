93c51797b4c916ae78a42bab8ff3060506cdad6c
Matthew Barrowclough <mbarrowclough@hotmail.com>
2013-12-09 21:43:55 -0700

    Paperclip image upload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,5 @@ Pinteresting::Application.configure do
 
  #required for heroku
  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+ Paperclip.options[:command_path] = "c/Program Files/ImageMagick-6.8.7-Q16/convert/"
 end
